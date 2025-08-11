33c5f9186be91ce2a471237aa3201064531bd7cf
maulrman <maulana.auliar@gmail.com>
2025-08-11 09:21:52 +0700

    DONE 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 
 const Filter = ({ value, onChange }) => {
   return (
@@ -35,12 +37,7 @@ const Persons = ({ persons }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
@@ -84,6 +81,14 @@ const App = () => {
     )
     setFilterPersons(contacts)
   }
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(resp => {
+        setPersons(resp.data)
+      })
+  },[])
 
   if (filterName !== ''){
     return (
