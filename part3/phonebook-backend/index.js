b25cc281a0a671f8e56c2a0bce0460e6d947ee76
maulrman <maulana.auliar@gmail.com>
2025-08-20 06:36:56 +0700

    added cors

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.use(express.json())
 app.use(morgan('combined'))
+app.use(cors())
 
 let phonebook_data = [
     { 
