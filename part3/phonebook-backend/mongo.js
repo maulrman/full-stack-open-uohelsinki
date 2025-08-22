0d2cfdeaef17066370bb7d8ecd0383a3bfcd46d7
maulrman <maulana.auliar@gmail.com>
2025-08-22 21:03:34 +0700

    connect to mongodb

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const mongo_user = process.env.MONGO_DB_USER
+const mongo_pass = process.env.MONGO_DB_PASS
+const mongo_cluster_uri = process.env.MONGO_CLUSTER_URI
+const mongo_url = `mongodb+srv://${mongo_user}:${mongo_pass}@${mongo_cluster_uri}`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(mongo_url)
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length <= 2){
+    Person
+        .find({})
+        .then(res => {
+            if (res.length <= 0) {
+                console.log('phonebook is empty')
+            } else {
+                console.log('phonebook:')
+                res.forEach(person => {
+                    console.log(`${person.name} ${person.number}`)
+                })
+            }
+            mongoose.connection.close()
+        })
+} else {
+    const new_person = new Person({
+        name: process.argv[2],
+        number: process.argv[3]
+    })
+    new_person
+        .save()
+        .then(res => {
+            console.log('person saved.')
+            mongoose.connection.close()
+        })
+}
